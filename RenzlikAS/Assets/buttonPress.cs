1fdd57d71989b690d0c12637d6cb31a36f2633a4
Bregul <Bregul@users.noreply.github.com>
2019-03-23 16:03:50 +0100

    FileBrowser - działający przycisk
    bo naciśnięciu przycisku w file Browser wyświetla on informację w konsoli.

---
+++ RenzlikAS/Assets/buttonPress.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buttonPress : MonoBehaviour
+{
+
+    public Button chooseFile;
+     // Start is called before the first frame update
+    void Start()
+    {
+        chooseFile.onClick.AddListener(onClick);
+    }
+
+    void onClick()
+    {
+        Debug.Log("CLICK");
+    }
+}
