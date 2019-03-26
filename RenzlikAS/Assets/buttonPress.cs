5c0827e73fba808ca589da3700e33b16edab0364
Bregul <Bregul@users.noreply.github.com>
2019-03-26 15:11:26 +0100

    Update buttonPress.cs

--- RenzlikAS/Assets/buttonPress.cs
+++ RenzlikAS/Assets/buttonPress.cs
@@ -1,38 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using UnityEditor;
+﻿using UnityEngine;
+using UnityEngine.UI;  // obsługa guzika
+using UnityEditor;  // Pozwala nam dodać EditorUtility, z pomocą którego otwieramy panel wyboru pliku, oraz panel błędu.
 
 public class buttonPress : MonoBehaviour
 {
     public GameObject Audio_Source;
-    string File_Path, File_Extension;
-    public Button Choose_File;
+    string File_Path, File_Extension;  // stringi deklarujące lokalizację, oraz rozszerzenie pliku audio
+    public Button Choose_File;  // dodajemy przycisk do sceny
     
 
 
     void Start()
     {
-        Choose_File.onClick.AddListener(On_Click);
+        Choose_File.onClick.AddListener(On_Click); // button "czeka" na naciśnięcie przez użytkownika, oraz po naciśnięciu wykonuje metodę On_Click
     }
 
     void On_Click()
     {
-        Select_File();
-        Debug.Log("click");
+        Select_File(); // instrukcje po naciśnięciu buttona
     }
 
     public void Select_File()
     {
-        File_Path = EditorUtility.OpenFilePanel("Wybierz utwór", "", "");
-        File_Extension = File_Path.Substring(File_Path.IndexOf('.') + 1);
-        while (File_Extension != "mp3")
+        File_Path = EditorUtility.OpenFilePanel("Wybierz utwór", "", "");  // otwiera okno przeglądania plików, w nawiasach wpisujemy ("nazwa okna","lokalizacje(?)","rozszerzenia")
+        File_Extension = File_Path.Substring(File_Path.IndexOf('.') + 1); // pozwala nam określić rozszerzenie pliku poprzez znalezienie kropki w nazwie pliku, oraz zwrócenie nam wszystkiego co się znajduje za nią
+        while (File_Extension != "mp3") // pętla sprawdzająca rozszerzenie pliku
         {
-            EditorUtility.DisplayDialog("Error", "Niepoprawne rozszerzenie\nWybierz jeszcze raz", "OK", "");
-            File_Path = EditorUtility.OpenFilePanel("Wybierz utwór", "", "");
-            File_Extension = File_Path.Substring(File_Path.IndexOf('.') + 1);
+            EditorUtility.DisplayDialog("Error", "Niepoprawne rozszerzenie\nWybierz jeszcze raz", "OK", ""); // error 
+            File_Path = EditorUtility.OpenFilePanel("Wybierz utwór", "", ""); // ponowny wybór pliku
+            File_Extension = File_Path.Substring(File_Path.IndexOf('.') + 1); // ponowna analiza rozszerzenia
         }
-        Debug.Log(File_Path);
+        Debug.Log(File_Path); // zwraca nam ścieżkę pliku w konsoli
     }
 }
