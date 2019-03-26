ca55ec2a637142d666a826d6225387312be93878
Bregul <Bregul@users.noreply.github.com>
2019-03-26 13:50:35 +0100

    FileBrowser- okienko
    Po naciśnięciu guzika "Choose File" otwiera nam się okno w którym możemy wybrać plik audio, oraz zwraca nam ścieżkę pliku.

--- RenzlikAS/Assets/buttonPress.cs
+++ RenzlikAS/Assets/buttonPress.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEditor;
 
 public class buttonPress : MonoBehaviour
 {
+    public string path;
     public Button chooseFile;
      // Start is called before the first frame update
     void Start()
@@ -14,6 +16,13 @@ public class buttonPress : MonoBehaviour
 
     void onClick()
     {
+        FileSelect();
         Debug.Log("click");
+    }
+
+    public void FileSelect()
+    {
+        path = EditorUtility.OpenFilePanel("Wybierz utwór", "", "mp3");
+        print(path);
     }
 }
