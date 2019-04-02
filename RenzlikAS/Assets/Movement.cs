780589ebd5c2ba1f14a89d5477f11acd2bdd1ba3
Bregul <Bregul@users.noreply.github.com>
2019-04-02 15:28:42 +0200

    RenzlikAS

--- RenzlikAS/Assets/Movement.cs
+++ RenzlikAS/Assets/Movement.cs
@@ -1,12 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class movement : MonoBehaviour
 {
 
     public float speed;
-
     private Rigidbody rb;
+    string sceneName;
 
     void Start()
     {
@@ -15,11 +16,16 @@ public class movement : MonoBehaviour
 
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        Scene currentScene = SceneManager.GetActiveScene();
+        sceneName = currentScene.name;
+        if (sceneName != "File Browser")
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        rb.AddForce(movement * speed);
+            rb.AddForce(movement * speed);
+        }
     }
 }
