e44267ef2dfb62c76a268a98e2caf4569dc4011e
KKan98 <48765813+KKan98@users.noreply.github.com>
2019-04-09 17:00:07 +0200

    CameraSwitch360
    Mozna patrzeć do góry i w dół, trzeba jeszcze zmienić, aby obiekt obracał się wraz z kamerą, zniweluje to odwrócone kontrolowanie gdy kamera patrzy do tyłu

--- RenzlikAS/Assets/Scripts/CameraFollow.cs
+++ RenzlikAS/Assets/Scripts/CameraFollow.cs
@@ -4,15 +4,15 @@ public class CameraFollow : MonoBehaviour
 {
     private Camera myCamera ; 
     public Transform cameraAudioSource;
-    public float smoothSpeed = 0.125f;
-    public Vector3 offset;
-    private void FixedUpdate()
+    public float smoothSpeed = 0.125f; // prędkość poruszania się kamery za obiektem
+    public Vector3 offset; // pozwala na ustawienie offsetu, oddalenia od położenia kamery, robi się to w AudioController->Camera->Camera Follow(Script)->offset
+    private void FixedUpdate() 
     {
-        transform.position = cameraAudioSource.position+offset;
+        transform.position = cameraAudioSource.position+offset; // ciagle zczytuje pozycje kamery
     }
     private void Start()
     {
         myCamera = GetComponent<Camera>();
     }
-    
+
 }
---
+++ RenzlikAS/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    
+
+    private Vector2 rotation = Vector2.zero; // pozwala nam wykonywać rotacje wokół obiektu z offsetem równym (0,0)
+    public float lookSpeed = 3; // prędkość ruszania kamerą wokół obiektu
+
+    private void FixedUpdate()  
+    {
+        rotation.y += Input.GetAxis("Mouse X");
+        rotation.x += -Input.GetAxis("Mouse Y");
+        rotation.x = Mathf.Clamp(rotation.x, -15f, 15f);//clamp zwraca wartosc value jeżeli znajduje się w przedziale (float value, float min, float max)
+        transform.localRotation = Quaternion.Euler(rotation.x * lookSpeed, rotation.y*lookSpeed, 0); //obraca kamere wokół osi x i y, quaternion representuje rotacje 
+       
+    }
+    
+
+
+}
